d657fd36b860b6ed816208423697207598882788
Yash <yashlohia105@gmail.com>
2024-06-22 06:51:41 +0530

    Mount listing router

--- src/app.mjs
+++ src/app.mjs
@@ -1,14 +1,18 @@
 import express from 'express';
 import morgan from 'morgan';
+import listingRouter from './entities/listing/listingRoutes.mjs';
 
 const app = express();
 
 app.use(morgan('dev'));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.status(200).json({
     status: 'success',
   });
 });
+
+app.use('/api/listings', listingRouter);
 
 export default app;
