f81cdb0cef8929373befd203cc4fba88f3d537fd
Yash <yashlohia105@gmail.com>
2024-06-23 14:42:41 +0530

    Create and jwt token

--- src/entities/user/authController.mjs
+++ src/entities/user/authController.mjs
@@ -1,14 +1,39 @@
+import { promisify } from 'util';
+import jwt from 'jsonwebtoken';
 import AppError from '../../utils/AppError.mjs';
 import catchAsync from '../../utils/catchAsync.mjs';
 import User from './userModel.mjs';
 
+const createSendToken = async (res, user, statusCode) => {
+  const jwtSignAsync = promisify(jwt.sign);
+  const token = await jwtSignAsync({ id: user.id }, process.env.JWT_SECRET, {
+    expiresIn: '3d',
+  });
+
+  const cookieOptions = {
+    maxAge: 3 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === 'production') {
+    cookieOptions.sameSite = 'none';
+    cookieOptions.secure = true;
+  }
+
+  res.cookie('jwt', token, cookieOptions);
+
+  user.password = undefined;
+  res.status(statusCode).json({
+    status: 'success',
+    token,
+    user,
+  });
+};
+
 export const signup = catchAsync(async (req, res, next) => {
   const user = await User.create(req.body);
 
-  res.status(201).json({
-    status: 'success',
-    user,
-  });
+  await createSendToken(res, user, 201);
 });
 
 export const login = catchAsync(async (req, res, next) => {
@@ -31,10 +56,5 @@ export const login = catchAsync(async (req, res, next) => {
     return next(new AppError('Incorrect password.', 401));
   }
 
-  // Send JWT token
-
-  res.status(200).json({
-    status: 'success',
-    user,
-  });
+  await createSendToken(res, user, 200);
 });
