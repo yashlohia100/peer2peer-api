eaa9044a286f799b5cc6748cb958156c99b3f579
Yash <yashlohia105@gmail.com>
2024-06-23 11:41:26 +0530

    Use catchAsync

--- src/entities/listing/listingController.mjs
+++ src/entities/listing/listingController.mjs
@@ -1,6 +1,8 @@
+import AppError from '../../utils/AppError.mjs';
+import catchAsync from '../../utils/catchAsync.mjs';
 import Listing from './listingModel.mjs';
 
-export const getAllListings = async (req, res, next) => {
+export const getAllListings = catchAsync(async (req, res, next) => {
   const listings = await Listing.find();
 
   res.status(200).json({
@@ -8,22 +10,26 @@ export const getAllListings = async (req, res, next) => {
     numResults: listings.length,
     listings,
   });
-};
+});
 
-export const getListing = async (req, res, next) => {
-  const listing = await Listing.findById(req.params.id);
-
-  res.status(200).json({
-    status: 'success',
-    listing,
-  });
-};
-
-export const createListing = async (req, res, next) => {
+export const createListing = catchAsync(async (req, res, next) => {
   const listing = await Listing.create(req.body);
 
   res.status(201).json({
     status: 'success',
     listing,
   });
-};
+});
+
+export const getListing = catchAsync(async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+
+  if (!listing) {
+    return next(new AppError('No listing found with that id.', 404));
+  }
+
+  res.status(200).json({
+    status: 'success',
+    listing,
+  });
+});
