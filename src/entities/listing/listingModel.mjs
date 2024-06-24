705079d0d0ee8da17cf73691ee88a876be6ba4d0
Yash <yashlohia105@gmail.com>
2024-06-24 07:34:17 +0530

    Add fields to listing model

--- src/entities/listing/listingModel.mjs
+++ src/entities/listing/listingModel.mjs
@@ -1,9 +1,21 @@
 import { Schema, model } from 'mongoose';
+import {
+  cities,
+  facilities,
+  highlightsRoom,
+  highlightsRoommate,
+  preferences,
+} from './listingValidators.mjs';
 
 const listingSchema = new Schema({
   location: {
     type: String,
     required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+    enum: cities,
   },
   lookingFor: {
     type: String,
@@ -19,6 +31,32 @@ const listingSchema = new Schema({
     type: Number,
     required: true,
     min: 0,
+  },
+  listingType: {
+    type: String,
+    required: true,
+    enum: ['room', 'roommate'],
+  },
+  preferences: {
+    type: [String],
+    required: true,
+    enum: preferences,
+  },
+  facilities: {
+    type: [String],
+    enum: facilities,
+  },
+  highlights: {
+    type: [String],
+    required: true,
+    enum: [...highlightsRoom, ...highlightsRoommate],
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  images: {
+    type: [String],
   },
 });
 
---
+++ src/entities/listing/listingValidators.mjs
@@ -0,0 +1,56 @@
+export const preferences = [
+  'night owl',
+  'early bird',
+  'studious',
+  'fitness freak',
+  'sporty',
+  'wanderer',
+  'party lover',
+  'pet lover',
+  'vegan',
+  'non alcoholic',
+  'music lover',
+  'non smoker',
+];
+
+export const facilities = [
+  'power backup',
+  'tv',
+  'fridge',
+  'kitchen',
+  'wifi',
+  'machine',
+  'ac',
+  'cook',
+  'parking',
+];
+
+export const highlightsRoommate = [
+  'college student',
+  'working full time',
+  'working night shifts',
+  'age less than 25',
+  'age greater than 25',
+  'have 2 wheeler',
+  'have 4 wheeler',
+  'have pets',
+  'vegetarian',
+  'need no furnishing',
+];
+
+export const highlightsRoom = [
+  'market nearby',
+  'close to metro station',
+  'gym nearby',
+  'park nearby',
+  'public transport nearby',
+  'attached washroom',
+  'attached balcony',
+  'gated society',
+  'no restriction',
+  'newly built',
+  'separate washrooms',
+  'house keeping',
+];
+
+export const cities = ['ghaziabad', 'noida', 'delhi', 'gurugram'];
